f97d510b10ade583e0d717d68539f4b7735d0e59
Manuel Gomez <magomezca4@acciona.com>
2025-01-29 15:53:05 +0100

    Fix deployment error

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/ThreeJS_Experiments/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+}) 
